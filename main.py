62feefb2b988a5b22e1c21d899b37f374e5ff0cf
Xander Clairmon <choutte.solaire@proton.me>
2025-10-01 20:06:00 +0400

    Update project: перенастройка гит

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.include_router(me.router, prefix="/me")
 app.include_router(predict.router, prefix="/predict")
 app.include_router(history.router, prefix="/history")
 app.include_router(datasets.router, prefix="/datasets")
-app.include_router(lablog.router, prefix="/lablog", tags=["Лабораторный журнал"])
+app.include_router(lablog.router, prefix="/lablog", tags=["Лабораторный журнал!"])
 
 if __name__ == "__main__":
     import uvicorn
