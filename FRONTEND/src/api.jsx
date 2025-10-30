07796fada8c14cc0eeeacb66c660145ffbcddd76
Xander Clairmon <chouette.solaire@proton.me>
2025-10-30 16:13:51 +0400

    Update api.jsx

--- FRONTEND/src/api.jsx
+++ FRONTEND/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://26.132.93.41:8000",
+    baseURL: "http://26.***.**.**:8000",
 });
 
 // Перехватчик для добавления токена в заголовок Authorization
