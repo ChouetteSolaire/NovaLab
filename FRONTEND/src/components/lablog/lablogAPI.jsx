9103f243fa8649948d32558cc48728ad284780f6
Xander Clairmon <chouette.solaire@proton.me>
2026-02-14 17:24:55 +0400

    Delete FRONTEND directory

--- FRONTEND/src/components/lablog/lablogAPI.jsx
+++
@@ -1,9 +0,0 @@
-import API from './../../api';
-
-export const getLabLog = () => API.get('/lablog');
-
-export const createLabLogEntry = (data) => API.post('/lablog', data);
-
-export const updateLabLogEntry = (id, data) => API.put(`/lablog/${id}`, data);
-
-export const deleteLabLogEntry = (id) => API.delete(`/lablog/${id}`);
